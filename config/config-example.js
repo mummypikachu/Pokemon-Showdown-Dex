c5df61ad3bb7efe25b873d742e7326c74e963e61
mummypikachu <83767883+mummypikachu@users.noreply.github.com>
2023-05-31 11:44:05 -0400

    Update config-example.js

--- config/config-example.js
+++ config/config-example.js
@@ -1,5 +1,5 @@
 exports.routes = {
     root: 'pokemonshowdown.com',
-    client: 'play.pokemonshowdown.com',
+    client: 'showdown-server.herokuapp.com',
     replays: 'replay.pokemonshowdown.com',
 };
